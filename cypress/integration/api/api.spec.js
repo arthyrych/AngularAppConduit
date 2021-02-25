2284616b2d0928fe75e3bde9210e05abed3931b4
arthyrych <arthyrych@arthyrych.SOT.COM>
2021-02-25 19:04:15 +0200

    api test added

--- cypress/integration/api/api.spec.js
+++ cypress/integration/api/api.spec.js
@@ -1,10 +1,30 @@
+/// <reference types="Cypress" />
+
 describe('BE test suite', ()=> {
 
     beforeEach(()=> {
         cy.loginToApp()
     })
 
-    it('should log in', ()=> {
-        cy.log('Yeaah!')
+    it('verify request and response', ()=> {
+
+        cy.server()
+        cy.route('POST', '**/articles').as('postArticles')
+
+        cy.contains('New Article').click()
+        cy.get('[formcontrolname="title"]').type('This is a title!')
+        cy.get('[formcontrolname="description"]').type('This is an article about!')
+        cy.get('[formcontrolname="body"]').type('This is content!')
+        cy.contains(' Publish Article ').click()
+
+        cy.wait('@postArticles')
+        cy.get('@postArticles').then( xhr => {
+            console.log(xhr)
+            expect(xhr.status).to.equal(200)
+            expect(xhr.request.body.article.body).to.equal('This is content!')
+            expect(xhr.response.body.article.description).to.equal('This is an article about!')
+        })
+       
     })
+
 })
