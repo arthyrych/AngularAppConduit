ec07204d3c6ccf0925671ef4e031b31d4220dec3
arthyrych <arthyrych@arthyrych.SOT.COM>
2021-03-03 09:29:13 +0200

    refactored

--- cypress/integration/api/1-request-response.spec.js
+++ cypress/integration/api/1-request-response.spec.js
@@ -8,15 +8,18 @@ describe('request-response test suite', ()=> {
 
     it('verify request and response', ()=> {
 
+        // starting Cypress server, listening for the api call, saving the call as an alias (global variable)
         cy.server()
         cy.route('POST', '**/articles').as('postArticles')
 
+        // creating a new article through ui
         cy.contains('New Article').click()
         cy.get('[formcontrolname="title"]').type('This is a title!')
         cy.get('[formcontrolname="description"]').type('This is an article about!')
         cy.get('[formcontrolname="body"]').type('This is content!')
         cy.contains(' Publish Article ').click()
 
+        // waiting for the response, taking the response as an object, asserting what we need
         cy.wait('@postArticles')
         cy.get('@postArticles').then( xhr => {
             console.log(xhr)
